3a444306719fab3ab01b190d7793ebe045d84cb2
Avaya <130426359+avaykhatry@users.noreply.github.com>
2025-08-23 22:45:20 +0545

    chore: make weather fetch logic dynamic

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
+  const lat = 27.71;
+  const lon = 85.32;
 
-    fetch("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m")
-    .then(Response => {
-        if (!Response.ok) throw new Error(`HTTP error: ${Response.status}`);
-    return Response.json();
-})
-    .then(data => {
+  fetch(
+    `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&hourly=temperature_2m`
+  )
+    .then((Response) => {
+      if (!Response.ok) throw new Error(`HTTP error: ${Response.status}`);
+      return Response.json();
+    })
+    .then((data) => {
         return console.log(data);
     })
-    .catch(error => console.error(`Error: ${error}`));
+    .catch((error) => console.error(`Error: ${error}`));
 });
